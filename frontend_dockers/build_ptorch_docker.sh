b59b9e79084b9807aa1348ae3b221c56456746f1
Sergiy Popovych <popovych@seungworkstation1003.princeton.edu>
2018-04-03 17:36:56 -0400

    tdock

--- frontend_dockers/build_ptorch_docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t seunglab/unet_timing:ptorch -f Dockerfile.ptorch .
-docker push seunglab/unet_timing:ptorch
--- frontend_dockers/build_tflow_docker_gpu.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t seunglab/unet_timing:tflow -f Dockerfile.tflow .
-docker push seunglab/unet_timing:tflow
