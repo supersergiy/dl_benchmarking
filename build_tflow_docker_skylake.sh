4cadb390d31a32b0661d6cfc367ad244975366de
supersergiy <sergiy.popovich@gmail.com>
2018-04-04 13:58:03 -0400

    skylake docker

--- build_tflow_docker_skylake.sh
+++ build_tflow_docker_skylake.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t seunglab/unet_timing:tflow_skylake -f Dockerfile.tflow_skylake .
+docker build -t seunglab/unet_timing:tflow_skylake -f Dockerfile.tflow.skylake .
 docker push seunglab/unet_timing:tflow_skylake
