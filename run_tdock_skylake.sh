32a588f155ec683e3e777dfeff45f7eab57e99e5
supersergiy <sergiy.popovich@gmail.com>
2018-04-04 20:24:40 -0400

    bug

--- run_tdock_skylake.sh
+++ run_tdock_skylake.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker pull seunglab/unet_timing:tflow_skylake
-docker run -it --rm --runtime=nvidia  seunglab/unet_timing:tflow_skylake bash
+docker run -it --rm seunglab/unet_timing:tflow_skylake bash
